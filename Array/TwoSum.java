bf2f9391709e3e17b18692e6a2e1bc12895f932f
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2025-07-06 23:57:27 +0530

    solved Two Sum

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -5,29 +5,9 @@ public class TwoSum {
         int arr[] = {3, 5, 1, 7, 8, 9};
         int target = 8;
         int found=0;
-//        int sum=0;
-//        int[] arrSum = new int[2];
-//        for (int i=0; i<arr.length; i++){
-//            for (int j=i+1; i<arr.length; i++){
-//                sum = arr[i] + arr[j];
-//                System.out.println(sum);
-//                if (sum == target){
-//                    arrSum[0] = arr[i];
-//                    arrSum[1] = arr[j];
-//                    found = 1;
-//                }
-//            }
-//        }
-//        if (found==1){
-//            System.out.println(arrSum[0] + ", "+ arrSum[1]);
-//        }
-//        else {
-//            System.out.println("Target not found");
-//        }
-
 
         for (int i=0; i<arr.length; i++){
-            for (int j=i+1; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++){
                 if (arr[i]+arr[j] == target){
                     System.out.println(arr[i] +", "+ arr[j]);
                     found=1;
