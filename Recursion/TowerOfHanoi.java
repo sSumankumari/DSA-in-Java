ac537f38efbdf3ea9655fd93727be3d107c318e1
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2025-07-31 23:54:05 +0530

    Tower of Hanoi problem

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void moveRings(int n, char src, char helper, char dest){
+        if (n == 1){
+            System.out.println("Moving ring "+ n +" from "+ src +" to "+ dest);
+            return;
+        }
+
+        moveRings(n-1, src, dest, helper);
+        System.out.println("Moving ring "+ n +" from "+ src +" to "+ dest);
+        moveRings(n-1, helper, src, dest);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int rings = sc.nextInt();
+
+        int rings = 4;
+
+        char src = 'A';
+        char dest = 'B';
+        char helper = 'C';
+
+        moveRings(rings, src, helper, dest);
+    }
+}
