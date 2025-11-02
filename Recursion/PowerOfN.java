4ab22edd80dbd2aa9e3b0324d938717be326fbdc
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2025-11-02 23:01:09 +0530

    Power of N using recursion

--- Recursion/PowerOfN.java
+++ Recursion/PowerOfN.java
@@ -4,20 +4,39 @@ import java.util.Scanner;
 
 public class PowerOfN {
     public static long power(int n, int p){
-        if (p==1) return n;
-        long res = power(n, p/2);
-        if (p%2==0){
-            return res*res;
+        if (p==0) return 1;
+        long res = n * power(n, p-1);
+        return res;
+    }
+    public static long powerOptimized(int x, int p){
+        if (p==0) return 1;
+        long res = powerOptimized(x, p/2) * powerOptimized(x, p/2);
+
+        if (p%2 != 0){
+            res = x * res;
         }
-        else {
-            return res*res*n;
+
+        return res;
+    }
+    public static long powerMoreOptimized(int x, int p){
+        if (p==0) return 1;
+        long halfPower = powerMoreOptimized(x, p/2);
+        long res = halfPower * halfPower;
+
+        if (p%2 != 0){
+            res = x * res;
         }
+
+        return res;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int p = sc.nextInt();
+        System.out.println(power(2, 10));
+        System.out.println(power(2, 7));
 
-        System.out.println(power(n, p));
+        System.out.println(powerOptimized(5, 6));
+        System.out.println(powerOptimized(5, 3));
+
+        System.out.println(powerMoreOptimized(4, 5));
+        System.out.println(powerMoreOptimized(4, 6));
     }
 }
